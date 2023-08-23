4b917d0337b4cf9c4cfe37e7048d9d4eecb2ebcb
Andi Emini <andi@starlabspro.com>
2023-08-23 18:07:27 +0200

    Style update

--- header.php
+++ header.php
@@ -6,7 +6,10 @@
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 		<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 		<link href="https://fonts.cdnfonts.com/css/roboto" rel="stylesheet">
-
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet">
+		
         <meta name="viewport" content="width=device-width,initial-scale=1">
 		<?php wp_head(); ?>
 	</head>
