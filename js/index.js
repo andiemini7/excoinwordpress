e14c4d00ebafc86af38993efad73ddd46b88c32a
Andi Emini <andi@starlabspro.com>
2023-09-22 16:34:03 +0200

    Module push

--- js/index.js
+++ js/index.js
@@ -22,4 +22,146 @@ function toggleFaq(element) {
       symbol.textContent = '-';
     }
   }
-  
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const buyTab = document.querySelector(".crypto_marketplace_tabs_buy");
+    const sellTab = document.querySelector(".crypto_marketplace_tabs_sell");
+    const buyContent = document.querySelector(".crypto_marketplace_buy");
+    const sellContent = document.querySelector(".crypto_marketplace_sell");
+
+    buyTab.addEventListener("click", function() {
+        buyTab.classList.add("active");
+        sellTab.classList.remove("active");
+        buyContent.classList.add("active");
+        sellContent.classList.remove("active");
+    });
+
+    sellTab.addEventListener("click", function() {
+        sellTab.classList.add("active");
+        buyTab.classList.remove("active");
+        sellContent.classList.add("active");
+        buyContent.classList.remove("active");
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  let exchangeRate = 1; // Default is 1 for USD
+  const coinDropdown = document.getElementById("coins");
+  const amountInput = document.getElementById("amount");
+  const currencyDropdown = document.getElementById("currency_type");
+  const totalElement = document.querySelector(".crypto_marketplace_buy_price_total_amount");
+  const transferFeeElement = document.getElementById("transfer_fee_percentage");
+  const coinAmountInput = document.getElementById("coin-amount");
+
+
+  // Function to update the exchange rate based on selected currency
+  async function updateExchangeRate() {
+      const currency = currencyDropdown.value;
+      if (currency === "eur") {
+          const response = await fetch('https://api.exchangerate-api.com/v4/latest/USD');
+          const data = await response.json();
+          exchangeRate = data.rates.EUR;
+      } else {
+          exchangeRate = 1; 
+      }
+  }
+
+function updateTotal() {
+    const selectedCoin = coinDropdown.options[coinDropdown.selectedIndex];
+    const selectedPriceUSD = parseFloat(selectedCoin.getAttribute("data-price"));
+    const selectedAmount = parseFloat(amountInput.value) || 0;
+    const coinAmount = parseFloat(coinAmountInput.value) || 0;
+
+    if (!selectedPriceUSD || (!selectedAmount && !coinAmount)) {
+        totalElement.textContent = ''; 
+        return;
+    }
+
+    const selectedPrice = selectedPriceUSD * exchangeRate;
+    const transferFeeRate = parseFloat(transferFeeElement.getAttribute("value")) / 100;
+
+    if (amountInput.getAttribute('data-user-changed') === 'true') {
+        const convertedCoinAmount = (selectedAmount / selectedPrice);
+        coinAmountInput.value = convertedCoinAmount.toFixed(4);
+    } else if (coinAmountInput.getAttribute('data-user-changed') === 'true') {
+        const convertedAmount = (coinAmount * selectedPrice);
+        amountInput.value = convertedAmount.toFixed(2);
+    }
+
+    let total;
+    if (amountInput.getAttribute('data-user-changed') === 'true') {
+        total = selectedAmount;
+    } else if (coinAmountInput.getAttribute('data-user-changed') === 'true') {
+        total = coinAmount * selectedPrice;
+    }
+
+    const totalWithFees = total + (total * transferFeeRate);
+    let currencySymbol = currencyDropdown.value === 'usd' ? '$' : '€';
+    totalElement.textContent = `${currencySymbol}${totalWithFees.toFixed(2)}`;
+}
+
+
+
+function updateSendRequestLink() {
+    // Get the base URL from the data-baseurl attribute
+    let baseUrl = document.getElementById('send_request').getAttribute('data-baseurl');
+
+    // Determine if the user is buying or selling based on the active tab class
+    let action = document.querySelector('.crypto_marketplace_tabs_buy').classList.contains('active') ? 'buy' : 'sell';
+
+    // Get the selected coin ID and amounts
+    const selectedCoinId = coinDropdown.value;
+    const selectedAmount = parseFloat(amountInput.value) || 0;
+    const coinAmount = parseFloat(document.getElementById("coin-amount").value) || 0; // Added coin-amount input
+
+    // Fetch the dynamic transfer fee and convert it to a fraction
+    const transferFeeRate = parseFloat(transferFeeElement.getAttribute("value")) / 100;
+
+    // Get the selected currency (usd or eur)
+    const selectedCurrency = currencyDropdown.value;
+
+    // Construct the new URL with named parameters
+    let newUrl = `${baseUrl}?type=${action}&coin_id=${selectedCoinId}&coin_amount=${coinAmount}&currency=${selectedCurrency}&transfer_fee_rate=${transferFeeRate}`;
+
+    // Update the 'href' attribute of the 'Send Request' button
+    document.getElementById('send_request').setAttribute('href', newUrl);
+}
+
+
+  // Listen to changes
+  coinDropdown.addEventListener("change", () => {
+    updateTotal();
+    updateSendRequestLink();
+});
+amountInput.addEventListener("input", () => {
+    amountInput.setAttribute('data-user-changed', 'true');
+    coinAmountInput.setAttribute('data-user-changed', 'false');
+    updateTotal();
+    updateSendRequestLink();
+});
+
+coinAmountInput.addEventListener("input", () => {
+    coinAmountInput.setAttribute('data-user-changed', 'true');
+    amountInput.setAttribute('data-user-changed', 'false');
+    updateTotal();
+    updateSendRequestLink();
+});
+
+
+currencyDropdown.addEventListener("change", async () => {
+    await updateExchangeRate();
+    updateTotal();
+    updateSendRequestLink();
+});
+// Add event listeners for when the buy/sell tabs change
+document.querySelector('.crypto_marketplace_tabs_buy').addEventListener("click", updateSendRequestLink);
+document.querySelector('.crypto_marketplace_tabs_sell').addEventListener("click", updateSendRequestLink);
+
+
+  // Call once to initialize
+  updateExchangeRate().then(updateTotal);
+});
+
--- modules/sellbuy_crypto.php
+++ modules/sellbuy_crypto.php
@@ -8,6 +8,25 @@
             break;
         }
     }
+
+$ids = "tether,bitcoin,litecoin,ethereum,tron,ripple";
+$url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=$ids";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_USERAGENT, 'YourAppName/1.0 (http://yourappname.com)');
+// Disable this line in production
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$json = curl_exec($ch);
+if(curl_errno($ch)){
+    echo 'Curl error: ' . curl_error($ch);
+}
+curl_close($ch);
+
+$coins = json_decode($json, true);
+    // var_dump($coins);
 ?>
 
 <section id="crypto" class="container">
@@ -17,55 +36,70 @@
                 <span class="crypto_marketplace_tabs_buy active">Buy Crypto</span>
                 <span class="crypto_marketplace_tabs_sell">Sell Crypto</span>
             </div>
-            <div class="crypto_marketplace_buy">
+            <div class="crypto_marketplace_buy active">
                 <div class="crypto_marketplace_buy_dropdowns">
                     <div class="crypto_marketplace_buy_dropdowns_coin">
                         <label for="coins">Coin</label>
-                        <select id="coins">
-                            <option value="bitcoin">Bitcoin</option>
-                            <option value="ethereum">Ethereum</option>
-                        </select>
-                        <div class="fee">
-                            <span class="fee_title">Buying fee</span>
-                            <span class="fee_price">$4</span>
+                        <div class="coin-amount">
+                            <select id="coins">
+                                <option disabled selected value>Coin</option>
+                                <?php foreach ($coins as $coin): ?>
+                                    <option value="<?php echo htmlspecialchars($coin['symbol']); ?>" data-price="<?php echo $coin['current_price']; ?>">
+                                        <?php echo htmlspecialchars($coin['name']); ?>
+                                    </option>
+                                <?php endforeach; ?>
+                            </select>
+                            <input id="coin-amount">
                         </div>
                     </div>
                     <div class="crypto_marketplace_buy_dropdowns_amount">
-                    <label for="amount">Amount</label>
-                        <select id="amount">
-                            <option value="usd">USD</option>
-                            <option value="eur">Euro</option>
-                        </select>
+                        <label for="amount">Amount</label>
+                        <div class="amount_inputs">
+                            <input id="amount">
+                            <select id="currency_type">
+                                <option value="usd">USD</option>
+                                <option value="eur">Euro</option>
+                            </select>
+                        </div>
                         <div class="fee">
                             <span class="fee_title">Transfer fee</span>
-                            <span class="fee_price">2%</span>
+                            <span id="transfer_fee_percentage" class="fee_price" value="<?= $module['transfer_fee']; ?>"><?= $module['transfer_fee']; ?>%</span>
                         </div>
                     </div>
                 </div>
                 <div class="crypto_marketplace_buy_price">
-                    <span class="crypto_marketplace_buy_price_title">Total:</span>
-                    <span class="crypto_marketplace_buy_price_amount">$2,680</span>
-                    <a href="#" class="site_button">Send Request</a>
+                    <div class="crypto_marketplace_buy_price_total">
+                        <span class="crypto_marketplace_buy_price_total_title">Total:</span>
+                        <span class="crypto_marketplace_buy_price_total_amount"></span>
+                    </div>
+                    <a href="#" class="site_button" id="send_request" data-baseurl="<?= $module['redirect_url_site']; ?>">Send Request</a>
                 </div>
             </div>
+
+            
         </div>
-        <div class="crypto_prices">
-            <div class="crypto_prices_info">
-                <img src="<?= get_template_directory_uri() ?>/images/Ethereum.png">
-                <div class="crypto_prices_info_title">
-                    <span class="shortcode">ETH</span>
-                    <span class="name">Ethereum</span>
+        <div class="crypto_items">
+        <?php $coins =  array_slice($coins, 0, 3);
+         foreach ($coins as $coin): ?>
+            <div class="crypto_prices">
+                <div class="crypto_prices_info">
+                    <img src="<?php echo htmlspecialchars($coin['image']); ?>">
+                    <div class="crypto_prices_info_title">
+                        <span class="shortcode"><?php echo htmlspecialchars($coin['symbol']); ?></span>
+                        <span class="name"><?php echo htmlspecialchars($coin['name']); ?></span>
+                    </div>
+                </div>
+                <div class="crypto_prices_value">
+                    <span class="crypto_prices_value_title">Price:</span>
+                    <span class="crypto_prices_value_amount">$<?php echo htmlspecialchars($coin['current_price']); ?></span>
+                </div>
+                <div class="crypto_prices_change">
+                    <span class="crypto_prices_change_title">Change</span>
+                    <img src="<?= get_template_directory_uri() ?>/images/arrow.png">
+                    <span class="crypto_prices_change_value"><?php echo htmlspecialchars($coin['price_change_percentage_24h']); ?>%</span>
                 </div>
             </div>
-            <div class="crypto_prices_value">
-                <span class="crypto_prices_value_title">Price:</span>
-                <span class="crypto_prices_value_amount">$156.83</span>
-            </div>
-            <div class="crypto_prices_change">
-                <span class="crypto_prices_change_title">Change</span>
-                <img src="<?= get_template_directory_uri() ?>/images/arrow.png">
-                <span class="crypto_prices_change_value">-0.9%</span>
-            </div>
-        </div>
+        <?php endforeach; ?>
+         </div>
     </div>
 </section>
